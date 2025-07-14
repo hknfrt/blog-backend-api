9488cf2f658059733fa9a171eef1bcceaa2f5fdb
hknfrt <hknfrtt@proton.me>
2025-07-14 15:04:46 +0300

    Kullanıcı kaydı işlevi güncellendi; giriş verileri doğrulama, kullanıcı varlığını kontrol etme, şifreleme ve JWT oluşturma işlemleri eklendi. Hata yönetimi ile birlikte başarılı kayıt sonrası yanıt yapısı güncellendi.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,23 +3,95 @@ import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import { asyncWrapProviders } from "async_hooks"
+import { error } from "console"
 
 // Prisma client instance
 const prisma = new PrismaClient()
 
-//Register function
+// Register function
 export const register = async (req:Request, res:Response) => {
 
-    res.json({message:"Register Function Response"})
+    try {
+        // Get inputs
+    const {email, username, password} = req.body;
+
+
+    // Validation input
+    if(!email || !username || !password) {
+        return res.status(400).json({
+            error: "Email, username and password are require"
+        })
+    };
+
+    if(password.length<6){
+        res.status(400).json({
+            error: "Password must be at least 6 characters"
+        });
+    };
+
+    //Check user is exist
+    const existingUser = await prisma.user.findFirst({
+        where:{
+            OR:[
+                {email:email},
+                {username: username}
+            ]
+        }
+    });
+
+    if(existingUser){
+        return res.status(400).json({
+            error:"This username or email already exist"
+        });
+    };
+
+    // Hash password
+    const saltRounds = 10;
+    const hashedPassword =  await bcrypt.hash(password, saltRounds);
+
+    // Save to DB
+    const newUser = await prisma.user.create({
+        data: {
+            email,
+            username,
+            password:hashedPassword
+        }
+    });
+
+    // Create JWT token
+    const token = jwt.sign({userId:newUser.id}, process.env.JTW_SECRET!,{expiresIn: "7d"});
+
+    //Response to Frontend
+    const userResponse = {
+        id: newUser.id,
+        email: newUser.email,
+        username: newUser.username,
+        createdAt: newUser.createdAt
+    };
+
+    res.status(201).json({
+        message:"User added successfully",
+        user: userResponse,
+        token
+    });
+
+    } catch (error) {
+        console.error("Register error: ", error)
+        res.status(500).json({
+            error:"Server Error"
+        });
+        
+    };
+    
 }
 
-//Login function
+// Login function
 export const login = async(req:Request, res:Response) => {
     
     res.json({message:"Login Function Response"})
 }
 
-//Get current user info 
+// Get current user info 
 export const getCurrentUser = async(req:Request, res:Response) => {
 
     res.json({message:"GetCurrentUser Function Response"})
