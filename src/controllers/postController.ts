5507eb17e420c0d4ee37873afc7e3e7b73272383
hknfrt <hknfrtt@proton.me>
2025-07-21 11:50:17 +0300

    createPost işlevi eklendi; yazı oluşturma sürecinde başlık ve içerik doğrulaması yapıldı, hata yönetimi ve yanıt yapısı iyileştirildi. Başarılı oluşturma durumunda yeni yazı bilgileri ile birlikte yanıt döndürülüyor.

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -11,8 +11,60 @@ interface AuthRequest extends Request {
 
 // Create new post
 export const createPost = async (req: AuthRequest, res: Response) => {
-    // Burayı sonraki adımda dolduracağız
-    res.json({ message: "Create post function - henüz boş" });
+   try {
+    const {title, content, published} = req.body;
+    const userId = req.userId; // this comes from middleware
+    
+    // Validation
+    if(!title || !content) {
+        return res.status(400).json({
+            error:"Title and content are required"
+        });
+    }
+
+    if(title.trim().length<3){
+        return res.status(400).json({
+            error:"Title must be at least 3 characters"
+        });
+    }
+    
+    if(content.trim().length <10){
+        return res.status(400).json({
+            error:"Content must be at least 10 characters"
+        });
+    }
+
+    // Create post
+    const newPost = await prisma.post.create({
+        data:{
+            title: title.trim(),
+            content: content.trim(),
+            published: published || false, // Default: false(draft)
+            authorId: userId!
+        },
+        include:{
+            author:{
+                select:{
+                    id:true,
+                    username:true,
+                    email:true
+                }
+            }
+        }
+    });
+
+    res.status(201).json({
+        message:"Post create succsessfully",
+        post: newPost
+    })
+
+   } catch (error) {
+    console.error("Create post error: ", error)
+    res.status(500).json({
+        error:"Server Error"
+    })
+    
+   }
 };
 
 // Get all posts (public - herkes görebilir)
